671faef33892d907cef6e31be56079de59713c6b
Shaik Aftab <shaikaftab005@gmail.com>
2024-09-17 22:12:57 +0530

    Stripe setup

--- lib/actions/index.ts
+++ lib/actions/index.ts
@@ -6,6 +6,7 @@ import { Prisma } from "@prisma/client";
 import { redirect } from "next/navigation";
 import prisma from "../db";
 import { PostSchema, siteSchema } from "../ZodSchema";
+import { stripe } from "@/utils/stripe";
 
 export async function CreateSiteAction(formData: FormData) {
   try {
@@ -308,4 +309,61 @@ export async function getUserSubscriptions(userId: string) {
   } catch (error) {
     throw new Error("Error getting user subscriptions");
   }
+}
+
+export async function CreateSubscription() {
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+  if (!user) {
+    return redirect("/api/auth/login");
+  }
+
+  let stripeUserId = await prisma.user.findUnique({
+    where: {
+      id: user.id,
+    },
+    select: {
+      customerId: true,
+      email: true,
+      firstName: true,
+    },
+  });
+
+  if (!stripeUserId?.customerId) {
+    const stripeCustomer = await stripe.customers.create({
+      email: stripeUserId?.email,
+      name: stripeUserId?.firstName,
+    });
+
+    stripeUserId = await prisma.user.update({
+      where: {
+        id: user.id,
+      },
+      data: {
+        customerId: stripeCustomer.id,
+      },
+    });
+  }
+
+  const session = await stripe.checkout.sessions.create({
+    customer: stripeUserId.customerId as string,
+    mode: "subscription",
+    billing_address_collection: "auto",
+    payment_method_types: ["card"],
+    line_items: [{ price: process.env.STRIPE_PRICE_ID, quantity: 1 }],
+    customer_update: {
+      address: "auto",
+      name: "auto",
+    },
+    success_url:
+      process.env.NODE_ENV === "production"
+        ? "https://blog-marshal.vercel.app/dashboard/payment/success"
+        : "http://localhost:3000/dashboard/payment/success",
+    cancel_url:
+      process.env.NODE_ENV === "production"
+        ? "https://blog-marshal.vercel.app/dashboard/payment/cancelled"
+        : "http://localhost:3000/dashboard/payment/cancelled",
+  });
+
+  return redirect(session.url as string);
 }
